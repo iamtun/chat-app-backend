48d60a0d329673295377abf767f5e7d82dc55e3f
iamtun <letuan19431791.iuh@gmail.com>
2024-06-09 19:22:33 +0700

    feat: create user router

---
+++ src/routers/index.ts
@@ -0,0 +1,8 @@
+﻿import {Router} from 'express';
+import userRouter from './user.router';
+
+const appRouter = Router();
+
+appRouter.use('/users', userRouter);
+
+export default appRouter;
---
+++ src/routers/user.router.ts
@@ -0,0 +1,7 @@
+﻿import { Router } from 'express';
+import userController from '../controllers/user.controller';
+import { AuthMiddleware } from '../middlewares';
+
+const router = Router();
+router.get('/', AuthMiddleware, userController.getUser);
+export default router;
