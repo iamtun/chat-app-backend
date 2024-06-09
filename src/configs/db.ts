55cac5a3c7eb03013e6df588d139d9f40e85e6bf
iamtun <letuan19431791.iuh@gmail.com>
2024-06-09 19:18:57 +0700

    feat: create configs

---
+++ src/configs/db.ts
@@ -0,0 +1,44 @@
+﻿import mongoose from 'mongoose';
+
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+
+class Database {
+	private static instance: Database;
+	private connection: mongoose.Connection | null = null;
+
+	private constructor() {
+		this.connect();
+	}
+
+	public static getInstance(): Database {
+		if (!Database.instance) {
+			Database.instance = new Database();
+		}
+		return Database.instance;
+	}
+
+	private connect() {
+		const uri = process.env.MONGO_DB_URL!; // Replace with your actual connection string
+		mongoose.connect(uri, {
+			user: process.env.MONGO_DB_USER!,
+			pass: process.env.MONGO_DB_PASS!,
+		});
+
+		this.connection = mongoose.connection;
+		this.connection.on('connected', () => {
+			console.info('Database connection successful');
+		});
+
+		this.connection.on('error', (err) => {
+			console.error('Database connection error:', err);
+		});
+	}
+
+	public getConnection(): mongoose.Connection | null {
+		return this.connection;
+	}
+}
+
+export default Database;
---
+++ src/configs/firebase-admin.ts
@@ -0,0 +1,12 @@
+﻿import admin from 'firebase-admin';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+
+const credential = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS!);
+
+admin.initializeApp({
+	credential: admin.credential.cert(credential),
+});
+
+export default admin;
