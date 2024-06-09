90cb5a1f049f8e037a25866f92b4365c67a4d0eb
iamtun <letuan19431791.iuh@gmail.com>
2024-06-09 19:21:47 +0700

    feat: create user controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,12 @@
+﻿import { Response, Request } from 'express';
+
+class UserController {
+	constructor() {}
+
+	async getUser(req: Request, res: Response) {
+		return res.status(200).json({ user: req.user });
+	}
+}
+
+const userController = new UserController();
+export default userController;
