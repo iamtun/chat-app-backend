4ebf07ca410818ae205286cef8b36e539f667e28
iamtun <letuan19431791.iuh@gmail.com>
2024-06-10 22:36:11 +0700

    feat: create storage

---
+++ src/configs/storage.ts
@@ -0,0 +1,24 @@
+﻿import { configDotenv } from 'dotenv';
+import multerS3 from 'multer-s3';
+import { S3Client } from '@aws-sdk/client-s3';
+
+configDotenv();
+
+const s3Client = new S3Client({
+	region: process.env.AWS_REGION!,
+	credentials: {
+		accessKeyId: process.env.AWS_ACCESS_KEY!,
+		secretAccessKey: process.env.AWS_SECRET_KEY!,
+	},
+});
+
+const S3Storage = multerS3({
+	s3: s3Client,
+	bucket: process.env.AWS_S3_BUCKET_NAME!,
+	contentType: multerS3.AUTO_CONTENT_TYPE,
+	key: function (_: any, file: any, cb: Function) {
+		cb(null, 'uploads/' + Date.now().toString() + '-' + file.originalname);
+	},
+});
+
+export default S3Storage;
