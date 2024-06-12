4426cbd901b5464aa214b240b17d604b2d8f5d54
iamtun <letuan19431791.iuh@gmail.com>
2024-06-13 06:47:53 +0700

    feat(users): add mapping response data

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+﻿import { Response } from 'express';
+
+const mappingResponse = (res: Response, statusCode: number, data: any) => {
+	return res.status(statusCode).json({ data });
+};
+
+export { mappingResponse };
