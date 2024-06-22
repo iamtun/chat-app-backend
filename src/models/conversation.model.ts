d91138bbc1602141bf73e572c894718a06f8c994
iamtun <letuan19431791.iuh@gmail.com>
2024-06-22 15:09:47 +0700

    feat(conversation): add models

---
+++ src/models/conversation.model.ts
@@ -0,0 +1,33 @@
+﻿import mongoose from 'mongoose';
+import { createModel, createSchema } from '.';
+
+const schemaProps = {
+	name: {
+		type: String,
+		default: null,
+	},
+	avatar: {
+		type: String,
+		default: null,
+	},
+	members: {
+		type: Array<mongoose.Schema.Types.ObjectId>,
+		ref: 'users',
+		default: [],
+	},
+	is_group: {
+		type: Boolean,
+		default: false,
+	},
+	last_message: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'messages',
+		default: null,
+	},
+};
+
+const conversationModel = createModel(
+	'conversations',
+	createSchema(schemaProps),
+);
+export default conversationModel;
---
+++ src/models/message.model.ts
@@ -0,0 +1,30 @@
+﻿import mongoose from 'mongoose';
+import { createModel, createSchema } from '.';
+
+const schemaProps = {
+	content: {
+		type: String,
+		default: null,
+	},
+	sender: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'users',
+		default: null,
+	},
+	files: {
+		type: Array<mongoose.Schema.Types.ObjectId>,
+		ref: 'files',
+		default: [],
+	},
+	conversation_id: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'conversations',
+	},
+	deletedAt: {
+		type: Date,
+		default: null,
+	},
+};
+
+const messageModel = createModel('messages', createSchema(schemaProps));
+export default messageModel;
