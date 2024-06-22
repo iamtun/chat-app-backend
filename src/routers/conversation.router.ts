6f7a11906e691a32d9b8b4d479f67180ccb89c9b
iamtun <letuan19431791.iuh@gmail.com>
2024-06-22 15:22:58 +0700

    feat(conversation): add routers

---
+++ src/routers/conversation.router.ts
@@ -0,0 +1,8 @@
+﻿import { Router } from 'express';
+import { AuthMiddleware } from '../middlewares';
+import conversationController from '../controllers/conversation.controller';
+
+const router = Router();
+
+router.get('/', AuthMiddleware, conversationController.getConversationList);
+export default router;
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,10 +2,15 @@
 import userRouter from './user.router';
 import friendRouter from './friend.router';
 import uploadRouter from './upload.router';
+import conversationRouter from './conversation.router';
+import messageRouter from './message.router';
+
 const appRouter = Router();
 
 appRouter.use('/users', userRouter);
 appRouter.use('/friends', friendRouter);
+appRouter.use('/conversations', conversationRouter);
 appRouter.use('/upload', uploadRouter);
+appRouter.use('/messages', messageRouter);
 
 export default appRouter;
