67261f6ac44a6fb9b7c392f513464446bab2afc9
iamtun <letuan19431791.iuh@gmail.com>
2024-06-22 15:08:52 +0700

    fear(conversation): add fnc getFileType

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,4 +4,26 @@ const mappingResponse = (res: Response, statusCode: number, data: any) => {
 	return res.status(statusCode).json({ data });
 };
 
-export { mappingResponse };
+const getFileType = (fileName: string) => {
+	const extension = fileName?.split('.')?.pop()?.toLowerCase();
+
+	if (!extension) {
+		return 'unknown';
+	}
+
+	const imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff'];
+	const videoExtensions = ['mp4', 'avi', 'mov', 'wmv', 'flv', 'mkv'];
+	const documentExtensions = ['pdf', 'doc', 'docx', 'txt', 'xls', 'xlsx'];
+
+	if (imageExtensions.includes(extension)) {
+		return 'image';
+	} else if (videoExtensions.includes(extension)) {
+		return 'video';
+	} else if (documentExtensions.includes(extension)) {
+		return 'docs';
+	} else {
+		return 'unknown';
+	}
+};
+
+export { mappingResponse, getFileType };
