38db33c02078a782e2000fc693a54a36598a368f
iamtun <letuan19431791.iuh@gmail.com>
2024-06-22 15:22:04 +0700

    feat(conversation): re-add middleware

---
+++ src/middlewares/conversation.middleware.ts
@@ -0,0 +1,28 @@
+﻿import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import conversationService from '../services/conversation.service';
+import { ConversationIdInValid, NotFound } from '../errors';
+
+const ConversationMiddleware = {
+	validateConversationId: async (
+		req: Request,
+		res: Response,
+		next: NextFunction,
+	) => {
+		const { conversationId } = req.params;
+		if (!conversationId) {
+			return next(new ConversationIdInValid('Invalid conversation id'));
+		}
+
+		const conversation = await conversationService.getConversationById(
+			conversationId,
+		);
+
+		if (!conversation) {
+			return next(new NotFound('Conversation not found'));
+		}
+		next();
+	},
+};
+
+export default ConversationMiddleware;
